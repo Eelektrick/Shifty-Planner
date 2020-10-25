728453737d74464b61a9ec5f5123c929cd043a04
srgreiick <samgreilick@gmail.com>
2020-10-25 14:29:50 -0600

    added routing to app js and made edits to server file

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Login from "./components/Login";
+import Example from "./pages/Gallery"
 // import Scheduler from "./pages/Scheduler";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Wrapper>
-        <Login />
-        {/* <Gallery /> */}
-        {/* <Scheduler /> */}
-      </Wrapper>
-      <Footer />
-    </div>
+
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+
+          <Route  exact path="/" component={Login} />
+          <Route  exact path="/example" component={Example} />
+         
+          {/* <Gallery /> */}
+          {/* <Scheduler /> */}
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 const express = require("express");
 const app = express(); 
-const mongoose = require("mongoose");
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
+const mongoose = require("mongoose");
+const shiftRoutes = express.Router();
 const PORT = process.env.PORT || 3001;
 
+let Shift = require('./models/shiftModel');
 
-// Define middleware here
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
-// const connection = mongoose.connection;
-// connection.once('open', function() {
-//     console.log("MongoDB database connection established successfully");
-// })
+//Define middleware here
+app.use(cors());
+app.use(bodyParser.json());
 
 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test"
+, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+
+
+
+
+shiftRoutes.route('/shift').get(function(req, res) {
+  Shift.find(function(err, shifts) {
+      if (err) {
+        
+          console.log(err);
+      } else {
+          res.json(shifts);
+      }
+  });
+});
+
+
 // Add routes, both API and view
-// app.use(routes)asdf;
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test");
-
+// app.use(routes);
+app.use('/shifts', shiftRoutes)
 // Start the API server /test /test
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
