27dd8bded0a83abef8c1a855f3e0374870cefe5b
AtimaB <nickzehn10@hotmail.com>
2020-10-26 19:46:35 -0600

    notification is up

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,17 @@ import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Login from "./components/Login";
 import Example from "./pages/Gallery";
-// import Scheduler from "./pages/Scheduler";
+import Scheduler from "./pages/Scheduler";
 
 function App() {
   return (
-
     <Router>
       <div>
         <Navbar />
         <Wrapper>
-
-          <Route  exact path="/" component={Login} />
-          <Route  exact path="/example" component={Example} />
-          
+          <Route exact path="/" component={Login} />
+          <Route exact path="/example" component={Example} />
+          <Route exact path="/scheduler" component={Scheduler} />
           {/* <Gallery /> */}
           {/* <Scheduler /> */}
         </Wrapper>
