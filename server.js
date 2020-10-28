c560fcb5d91da84b2fbdb4e0b2151bac5f2d31d8
srgreiick <samgreilick@gmail.com>
2020-10-27 20:54:13 -0600

    possible heroku deploy fix

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test",{useNewUrlParser: true, useUnifiedTopology: true });
 
 // Start the API server
 app.listen(PORT, function () {
