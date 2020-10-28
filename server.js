66871922faf9461b3498c9d4883431bd96e9f26a
srgreiick <samgreilick@gmail.com>
2020-10-27 21:43:50 -0600

    api call fix for local

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true } || "mongodb://localhost/test",{useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI|| "mongodb://localhost/shifty-planner",{useNewUrlParser: true, useUnifiedTopology: true });
 
 // Start the API server
 app.listen(PORT, function () {
