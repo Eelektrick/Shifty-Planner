ef41c33236a144020ba50ec437a3076f81573902
srgreiick <samgreilick@gmail.com>
2020-10-29 16:43:01 -0600

    functions fun request in place!

--- models/shiftModel.js
+++ models/shiftModel.js
@@ -20,8 +20,8 @@ const shiftModel = new mongoose.Schema
          required: true,
          trim: true,
          lowercase: true,
-         unique: true,
-         required: 'Email address is required',
+        //  unique: true,
+         required: 'Email address is ne',
          validate: [validateEmail, 'Please fill a valid email address'],
          match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
      },
