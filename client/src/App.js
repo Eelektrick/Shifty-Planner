0187260799dd9df83c5a8396d89d416b2c95c1c7
srgreiick <samgreilick@gmail.com>
2020-10-30 14:03:50 -0600

    begining of refresher, psudocode done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
-import Example from "./pages/Gallery";
+import Example from "./pages/Example";
 import Scheduler from "./pages/Scheduler";
 import HomePage from "./pages/HomePage";
 import License from "./pages/License";
@@ -15,11 +15,11 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <ProtectedRoute exact path="/" component={HomePage} />
-          <ProtectedRoute exact path="/example" component={Example} />
+          <ProtectedRoute exact path="/" component={HomePage}/>
+          <ProtectedRoute exact path="/example" component={Example}/>
           <ProtectedRoute exact path="/scheduler" component={Scheduler} />
-          <ProtectedRoute exact path="/home" component={HomePage} />
-          <ProtectedRoute exact path="/license" component={License} />
+          <ProtectedRoute exact path="/home" component={HomePage}/>
+          <ProtectedRoute exact path="/license" component={License}/>
         </Wrapper>
       </div>
     </Router>
--- client/src/components/CalendarRefresher/index.js
+++ client/src/components/CalendarRefresher/index.js
@@ -13,7 +13,23 @@ class CalenderRefresher extends Component {
   }
 
   componentDidMount() {
-    
+    //When "Generate Shift" is pressed.
+    //Then a popup with 2 options is displayed plus a submit button.
+    //options:
+    //  1 - Interval to determine when shift will occur/at what interval
+    //    1.a - allows for custom time frame, option to select classic A B C.
+    //  2 - Option to determine who will be on the shift (api call is made to check certification to dynamically verify
+    //      that crew is up to spec).
+    //    2.a Allows user to select a default where and api call will be made and select everyone who is defaulted to
+    //        that shift.
+    //When popup submit is pressed then the data is verified against the database and if it clears a post request will be 
+    // sent storing the new shift(s)
+
+
+
+
+
+
   }
 
   handleSelect = event => {
