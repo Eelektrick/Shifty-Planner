810100a4e9abe8fe2b151e47943a18fe8c9aac4a
Corey Post <corey.post0120@gmail.com>
2020-10-31 14:40:53 -0600

    added code to tell program where to go after logout + cleaned up app.js code

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -3,10 +3,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton =()=> {
     const { logout } = useAuth0();
+    const logoutWithRedirect = () =>
+    logout({
+      returnTo: window.location.origin
+    });
 
     return (
       <a
-        onClick={() => logout()}
+        // href=" https://scheduling.auth0.com/v2/logout?returnTo=https://shifty-planner.herokuapp.com/&client_id=TaNNdVgbilHR0s6eTd3aT1Gi7jhXKa5Z"
+        onClick={() => logoutWithRedirect()}
         style={{ color: "rgb(190, 147, 3)" }}
         className="nav-link"
       >
