d2b0dedb40379a0dd0fc731a6064adff2985caa9
srgreiick <samgreilick@gmail.com>
2020-11-01 16:22:56 -0700

    added fields for inputing shift information, created mock model of states to fillout for create/post request

--- client/src/components/CalendarRefresher/index.js
+++ client/src/components/CalendarRefresher/index.js
@@ -2,44 +2,86 @@ import React, { Component } from "react";
 import { Button, Form, Col } from "react-bootstrap";
 import API from "../../utils/API";
 
+
+
 class CalenderRefresher extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      shift: "",
-      interval:"",
-      
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        authID: "",
+        emailID: "",
+        shift: "",
+        start: 0,
+        end: 0,
+        traded: 1,
+        name : "",
+        ignoredLists : []
     };
+  
+      this.handleChangeName = this.handleChangeName.bind(this);
+      this.handleChangeTime = this.handleChangeTime.bind(this);
+      this.handleSubmitName = this.handleSubmitName.bind(this);
+      this.handleSubmitTime = this.handleSubmitTime.bind(this);
+    }
+  
+    handleChangeName(event) {
+      this.setState({name: event.target.value});
+      console.log(this.state.name);
+    }
+    handleSubmitName(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    handleChangeTime(event) {
+      this.setState({value: event.target.value});
+      event.preventDefault();
+    }
+    handleSubmitTime(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit(event) {
-    console.log(this.state.name);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+    handleSubmit(event){
+      alert("WHY DID YOU PUSH THE BUTTON");
+    }
+  
+    render() {
+      return (
+        <div>
+          {/* -------------SHIFT----------- */}
+        <form onSubmit={this.handleSubmit}>
+          <h2>Shift to be made</h2>
+          <label>
+           Shift:
+            <input type="text" value={this.state.start} onChange={this.handleChangeTime} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {/* ---------------NAME------------- */}
+        <form onSubmit={this.handleSubmit}>
+        <h2>Personnel on the shift </h2>
+          <label>
+            Name:
+            <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <form onSubmit={this.handleSubmit}>
+          {/* ---------Final Gen-------------- */}
+        <h2>Generate!</h2>
+          <label>
+            <div>Submit all   </div>
+          </label>
+          <input type="submit" value="DO NOT PUSH THE BUTTON" />
+        </form>
+        </div>
+      );
+    }
+  
 }
 
 export default CalenderRefresher;
+
+
